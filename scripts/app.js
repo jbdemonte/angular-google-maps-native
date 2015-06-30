22f0bae7c4cad47a45951f2f830030d3e7a2ba2a
jbd <jbdemonte@gmail.com>
2015-07-01 01:11:51 +0200

    Use Jekyll to use layout + append nav + append quickstart page

--- scripts/app.js
+++ scripts/app.js
@@ -1,117 +1,56 @@
-angular.module('project', [])
+angular.module('project', ['Snippets', 'SnippetsThemeBootstrapButtons'])
 
-  .directive('onLast', function ($timeout, $parse) {
-    return {
-      restrict: 'A',
-      link: function (scope, element, attrs) {
-        if (scope.$last === true) {
-          $timeout(function () {
-            $parse(attrs.onLast)(scope);
-          });
-        }
-      }
-    }
+  .config(function(snippetsProvider) {
+    snippetsProvider.content.before(
+      "<snippets-pane snippet='" + angular.toJson({
+        name: 'Run',
+        icon: 'fa fa-cog',
+        cls: 'pull-right',
+        run: true
+      }) + "'><iframe ng-if='current.snippet.run' ng-src='{{trustSrc(path)}}'></iframe></snippets-pane>"
+    );
   })
 
-  .directive('tabs', function() {
+  .controller('MyCtrl', function ($scope, $sce) {
+      $scope.trustSrc = function(src) {
+        return $sce.trustAsResourceUrl(src);
+      };
+  })
+
+  .directive('params', function () {
     return {
       restrict: 'E',
       transclude: true,
       scope: {},
+      templateUrl: 'partials/params.html',
       controller: function($scope) {
-        var first = true,
-          panes = $scope.panes = [];
-
-        $scope.select = function(pane) {
-          angular.forEach(panes, function(pane) {
-            pane.selected = false;
-          });
-          pane.selected = true;
-        };
-
-        this.addPane = function(pane) {
-          if (first || pane.selected) {
-            $scope.select(pane); // select at least first item (required if none are unavailable)
-            first = pane.error; // wait for first available
-          }
-          panes.push(pane);
-        };
-      },
-      template: '<ul class="nav-tabs">' +
-                  '<li ng-repeat="pane in panes" ng-class="{active:pane.selected, \'{{pane.align}}\': pane.align}" ng-attr-error="{{pane.error}}">' +
-                    '<a href="" ng-click="!pane.error && select(pane)"><i ng-if="pane.icon" class="fa {{pane.icon}}"></i>{{pane.title}}</a>' +
-                  '</li>' +
-                '</ul>' +
-                '<div class="tab-content" ng-transclude></div>'
-    };
-  })
-
-  .directive('pane', function() {
-    return {
-      require: '^tabs',
-      restrict: 'E',
-      transclude: true,
-      scope: {
-        title: '@',
-        error: '@',
-        icon: '@',
-        selected: '@',
-        align: '@'
-      },
-      link: function(scope, element, attrs, tabsCtrl) {
-        tabsCtrl.addPane(scope);
-      },
-      template: '<div class="tab-pane" ng-show="selected" ng-transclude></div>'
-    };
-  })
-
-  .directive('snippets', function ($templateRequest, $parse) {
-    return {
-      restrict: 'E',
-      template: '<tabs>' +
-                  '<pane ng-repeat="item in items" title="{{item.name}}" ng-attr-error="{{item.error}}" on-last="last()"><pre class="code language-{{item.type}}" ng-bind="item.content"></pre></pane>' +
-                  '<pane selected="true" ng-if="path" title="run" icon="fa-cog" align="right"><iframe ng-src="{{path}}"></iframe></pane>' +
-                '</tabs>',
-      link: function (scope, elem, attrs) {
-        var files = $parse(attrs.files)(scope),
-          done = 0,
-          items = [],
-          languages = { // these which needed to be renamed for prism (css => language-css : no need)
-            html: 'markup',
-            js: 'javascript'
-          };
-
-        scope.last = function () {
-          angular.forEach(elem.find("pre"), function (pre) {
-            Prism.highlightElement(pre);
-          });
-        };
-
-        if (attrs.path && files && files.length) {
-          angular.forEach(files, function (name) {
-            var ext = name.replace(/^.*\./, ''),
-              item = {name: name, type: languages[ext] || ext};
-            items.push(item);
-            $templateRequest('snippets/' + attrs.path + '/' + name, true)
-              .then(
-                function (content) {
-                  item.content = content;
-                },
-                function () {
-                  item.error = true;
-                }
-              )
-              .finally(function () {
-                done++;
-                if (done === files.length) {
-                  scope.items = items;
-                  scope.path = 'snippets/' + attrs.path + '/';
-                }
-              });
+        var items = $scope.items = [];
+        this.add = function(item) {
+          items.push(item);
+          items.sort(function (a, b) {
+            return a.name < b.name ? -1 : 1;
           });
         }
       }
-    }
+    };
   })
+
+  .directive('item', function () {
+    return {
+      restrict: 'E',
+      require: '^params',
+      link: function (scope, element, attrs, ctrl) {
+        var item = {
+          name: attrs.name,
+          types: (attrs.type || '').split(','),
+          optional: 'optional' in attrs,
+          contents: "<p>" + element.html() + "</p>"
+        };
+        element.remove();
+        ctrl.add(item);
+      }
+    };
+  })
+
 
 ;
