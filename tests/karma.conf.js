63f574848c84e294e3d90f4bbf490a6441959e95
jbd <jbdemonte@gmail.com>
2015-07-08 20:48:30 +0200

    add code coverage

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -30,13 +30,19 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'dist/angular-google-maps-native.min.js': ['coverage']
+    },
+
+    coverageReporter: {
+      type: 'lcovonly',
+      dir: 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
     // web server port
     port: 9876,
