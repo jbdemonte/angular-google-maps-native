0d33862c6cbd86ff1ae55b54153e9acdcf6a5bcc
jbd <jbdemonte@gmail.com>
2015-08-04 20:23:31 +0200

    update coverage target

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'dist/angular-google-maps-native.js': ['coverage']
+      'src/angular-google-maps-native.js': ['coverage']
     },
 
     coverageReporter: {
